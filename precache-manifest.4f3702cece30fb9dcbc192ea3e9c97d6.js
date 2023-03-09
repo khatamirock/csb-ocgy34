b98aa46f997df715e3b409472715618b9286be93
Code Sandbox <hello@codesandbox.io>
2023-03-09 13:08:29 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4f3702cece30fb9dcbc192ea3e9c97d6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f9222eecd33b71586938e17bcd291d",
+    "url": "https://khatamirock.github.io/csb-ocgy34/index.html"
+  },
+  {
+    "revision": "2b621e135df34e2aa394",
+    "url": "https://khatamirock.github.io/csb-ocgy34/static/js/2.46c8e90e.chunk.js"
+  },
+  {
+    "revision": "a1aa5c589cff21be46af",
+    "url": "https://khatamirock.github.io/csb-ocgy34/static/js/main.8fdd71fb.chunk.js"
+  },
+  {
+    "revision": "ec7b9c7b6f2801596168",
+    "url": "https://khatamirock.github.io/csb-ocgy34/static/js/runtime-main.47986f45.js"
+  }
+]);
